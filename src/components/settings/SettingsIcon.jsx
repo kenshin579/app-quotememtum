474aaa490e66f0f467f7cba10a02813e7a8e0038
kenshin579 <kenshin579@hotmail.com>
2020-09-21 17:32:54 +0900

    [QMT-1] components 파일 추가 - 재정리가 필요함

---
+++ src/components/settings/SettingsIcon.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import 'Images/settings.svg';
+import './css/settings.css';
+
+class SettingsIcon extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <a href="#" title="Settings" onClick={this.props.toggleSettingsModal.bind(this)}><img className="icon-top-left" src="./assets/images/settings.svg" alt="Settings" /></a>
+      </div>
+    );
+  }
+}
+
+export default SettingsIcon;
