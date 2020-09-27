ad3d5e672f3c8bdfa6b7863a9fc328c4018e4c94
kenshin579 <kenshin579@hotmail.com>
2020-09-27 16:22:13 +0900

    [QMT-4] 날짜 추가 (align 수정이 필요함)

--- src/components/clock/Clock.jsx
+++ src/components/clock/Clock.jsx
@@ -5,8 +5,18 @@ class Clock extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      time: this.currentMinSecs(this.props.clockFormat),
+      date: this.currentDate(),
+      time: this.currentMinSecs(this.props.clockFormat)
     };
+  }
+
+  currentDate() {
+      const currentTime = new Date();
+      const week = ['일', '월', '화', '수', '목', '금', '토'];
+      const dayOfWeek = week[currentTime.getDay()];
+      const month = currentTime.getMonth() + 1;
+      const date = currentTime.getDate();
+      return `${month} ${date} ${dayOfWeek}`
   }
 
   currentMinSecs(clockFormat) {
@@ -20,25 +30,32 @@ class Clock extends Component {
     return `${hrs < 10 ? `0${hrs}` : hrs}:${min < 10 ? `0${min}` : min}`;
   }
 
-  updateTime() {
+  updateTimeAndDate() {
     this.setState({ time: this.currentMinSecs(this.props.clockFormat) });
+    this.setState({ date: this.currentDate()});
   }
 
   componentDidMount() {
-    setInterval(this.updateTime.bind(this), 1000);
+    setInterval(this.updateTimeAndDate.bind(this), 1000);
   }
 
   componentWillReceiveProps(nextProps) {
     this.setState({
       time: this.currentMinSecs(nextProps.clockFormat),
     });
+      this.setState({
+          date: this.currentDate(),
+      });
   }
 
   render() {
     const timeArr = this.state.time.split(' ');
+    const dateArr = this.state.date.split(' ');
+
     return (
       <div className="clock">
-        <div className="time">{timeArr[0]}{timeArr[1] && <span className="ampm">{timeArr[1]}</span>}</div>
+        <div className="date">{dateArr[0]}/{dateArr[1]} ({dateArr[2]})</div>
+        <div className="time">{timeArr[0]}{timeArr[1] && <span className="ampm"> {timeArr[1]}</span>}</div>
       </div>
     );
   }
