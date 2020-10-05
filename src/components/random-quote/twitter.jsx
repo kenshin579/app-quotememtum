0ee58502c61136043849bcb9950725daa6dd0d4b
kenshin579 <kenshin579@hotmail.com>
2020-10-05 17:05:59 +0900

    [QMT-4] sns icon style 이름 변경

--- src/components/random-quote/twitter.jsx
+++ src/components/random-quote/twitter.jsx
@@ -5,7 +5,7 @@ const TwitterLink = (props) => {
   const tweetURL = `https://twitter.com/intent/tweet?text="${props.quoteText}"—${props.authorName}`;
   return (
     <div>
-      <a href={tweetURL} target='_blank'><img className="icon-quote" src='./assets/images/twitter.svg'/></a>
+      <a href={tweetURL} target='_blank'><img className="icon-sns" src='./assets/images/twitter.svg'/></a>
     </div>
   );
 };
