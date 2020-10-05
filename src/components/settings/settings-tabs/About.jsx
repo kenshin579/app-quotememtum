9998f53a22d28b51480e99b8c1c4bb48862d3d54
kenshin579 <kenshin579@hotmail.com>
2020-10-05 17:38:31 +0900

    [QMT-4] about 내용 작성중

--- src/components/settings/settings-tabs/About.jsx
+++ src/components/settings/settings-tabs/About.jsx
@@ -4,21 +4,17 @@ import '../css/about.css';
 class About extends React.Component {
   constructor(props) {
     super(props);
-
-    this.state = {
-    };
   }
 
   render() {
     return (
       <div className="about-settings-tab">
         <h1>About</h1>
-        <p>Clone of the Momentum chrome extension for desktop.<br></br>
+        <p>....<br/>
         Developed for <a target='_blank' rel="noopener noreferrer" href="https://chingu-cohorts.github.io/chingu-directory/">Chingu Cohort</a> &#34;Build to Learn&#34; project
         </p>
-        <p>Team Members:</p>
-        <p><a target='_blank' rel="noopener noreferrer" href="https://github.com/jrpcoder">@jrpcoder </a></p>
-        <p><a target='_blank' rel="noopener noreferrer" href="https://github.com/marigerr">@marigerr</a></p>
+        <p>Made by</p>
+        <p><a target='_blank' rel="noopener noreferrer" href="https://github.com/kenshin579">@kenshin579</a></p>
       </div>
     );
   }
