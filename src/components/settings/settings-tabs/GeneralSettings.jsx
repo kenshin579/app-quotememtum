f881166ec0abd1e7eee0f5b8162c87f36e02194c
kenshin579 <kenshin579@hotmail.com>
2020-10-07 23:54:43 +0900

    [QMT-15] 설정에 명언 업데이트 주기 옵션 추가

--- src/components/settings/settings-tabs/GeneralSettings.jsx
+++ src/components/settings/settings-tabs/GeneralSettings.jsx
@@ -21,25 +21,39 @@ class GeneralSettings extends React.Component {
     return `option-${(value === this.state.options.clockFormat) ? 'active' : 'default'}`;
   }
 
+  activeQuoteFrequency(value) {
+    return `option-${(value === this.state.options.quoteFrequency) ? 'active' : 'default'}`;
+  }
+
   render() {
     return (
       <div className="settings-main-pane-inner">
         <h1>보기 설정</h1>
-        <label className="show-option">
+        <label className="show-option-clock-freq">
           <span>SNS 공유 버튼</span>
           <Toggle id="showSns" defaultChecked={this.state.showFeatures.showSns}
             icons={false} onChange={this.props.toggleFeature.bind(this)} />
         </label>
         <br/>
         <h1>옵션</h1>
-        <label className="show-option">
+        <label className="show-option-clock-freq">
           <span>시계 포멧</span>
-          <div className="units-toggle-container">
+          <div className="units-clock-freq-toggle-container">
             <span id="clockFormat-12hour" className={this.activeClockFormat('12hour')} onClick={this.changeOption.bind(this)}>12시간</span>
             <span>|</span>
             <span id="clockFormat-24hour" className={this.activeClockFormat('24hour')} onClick={this.changeOption.bind(this)}>24시간</span>
           </div>
         </label>
+          <label className="show-option-quote-freq">
+              <span>명언 업데이트주기</span>
+              <div className="units-quote-freq-toggle-container">
+                  <span id="quoteFrequency-2hour" className={this.activeQuoteFrequency('2hour')} onClick={this.changeOption.bind(this)}>2시간</span>
+                  <span>|</span>
+                  <span id="quoteFrequency-6hour" className={this.activeQuoteFrequency('6hour')} onClick={this.changeOption.bind(this)}>6시간</span>
+                  <span>|</span>
+                  <span id="quoteFrequency-12hour" className={this.activeQuoteFrequency('12hour')} onClick={this.changeOption.bind(this)}>12시간</span>
+              </div>
+          </label>
       </div>
     );
   }
